1a7226fc918f18e26aa3b893a99a3d1e86d34491
Guillaume Laforge <glaforge@gmail.com>
2021-06-23 22:03:40 +0200

    Populate the dropdown with fruits from the backend

--- appengine/backend/src/main/static/js/app.js
+++ appengine/backend/src/main/static/js/app.js
@@ -2,4 +2,21 @@ console.log('App started');
 
 document.addEventListener("DOMContentLoaded", async function(event) {
     console.log('Document loaded');
+
+    try {
+        const fruitResp = await fetch('/fruit');
+        const fruits = await fruitResp.json();
+        console.log(`Loaded ${fruits.length} fruits`);
+
+        const fruitSelect = document.getElementById("fruit-select");
+        fruits.forEach(fruit => {
+            const item = document.createElement("sl-menu-item");
+            item.value = fruit.name;
+            item.textContent = `${fruit.name} (${fruit.calories} cal)`;
+            fruitSelect.appendChild(item);
+        });
+    } catch(e) {
+        console.log("Impossible to retrieve fruits");
+        console.error(e);
+    }
 });
